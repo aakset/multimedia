17462f07a2d130156db54918d794e62927e204f6
aakashkbw <114473332+aakashkbw@users.noreply.github.com>
2024-01-09 18:38:39 +0100

    About Page changed and rearranged

--- multimedia/src/About.js
+++ multimedia/src/About.js
@@ -7,43 +7,21 @@ import pic3 from "./onlinePhotos/skyline.jpg";
 function About() {
   return (
     <div className="container-about">
-      <div className="content">
-        <h1 className="titleAbout">About</h1>
-        <p className="mainText">
-          Welcome to Northlake Studios, a place where magic is made. At Northlake Studios, we offer a unique experience for musicians, producers, and artists. Our recording studios are equipped with state-of-the-art technology and top-notch equipment, providing the perfect environment for bringing your creative vision to life. Whether you're a seasoned professional or just starting your musical journey, our dedicated team is here to support you every step of the way. We pride ourselves on offering flexible rental options, customizable studio setups, and a comfortable space where you can focus on your craft. Join us at Northlake Studios and experience the perfect synergy between cutting-edge technology, exceptional sound quality, and unparalleled support. Book your studio session today and let your music soar to new heights!
-        </p>
-      </div>
+      <h1 className="titleAbout">About Northlake Studios</h1>
+      <p className="mainText">
+      Welcome to Northlake Studios, a place where magic is made. At Northlake Studios, we offer a unique experience for musicians, producers, and artists. Our recording studios are equipped with state-of-the-art technology and top-notch equipment, providing the perfect environment for bringing your creative vision to life. Whether you're a seasoned professional or just starting your musical journey, our dedicated team is here to support you every step of the way. We pride ourselves on offering flexible rental options, customizable studio setups, and a comfortable space where you can focus on your craft. Join us at Northlake Studios and experience the perfect synergy between cutting-edge technology, exceptional sound quality, and unparalleled support. Book your studio session today and let your music soar to new heights!        {}
+      </p>
 
       <div className="audio-container">
-        <div className="audio-player">
-          <div className="audio-content">
+        {[pic1, pic2, pic3].map((pic, index) => (
+          <div className="audio-player" key={index}>
             <audio controls>
-              <source src={require("./Song1.mp3")} type="audio/mpeg" />
+              <source src={require(`./Song${index + 1}.mp3`)} type="audio/mpeg" />
               Your browser does not support the audio element.
             </audio>
-            <img src={pic1} alt="Image 1" className="audio-image" />
+            <img src={pic} alt={`Image ${index + 1}`} className="audio-image" />
           </div>
-        </div>
-
-        <div className="audio-player">
-          <div className="audio-content">
-            <audio controls>
-              <source src={require("./Song2.mp3")} type="audio/mpeg" />
-              Your browser does not support the audio element.
-            </audio>
-            <img src={pic2} alt="Image 2" className="audio-image" />
-          </div>
-        </div>
-
-        <div className="audio-player">
-          <div className="audio-content">
-            <audio controls>
-              <source src={require("./Song3.mp3")} type="audio/mpeg" />
-              Your browser does not support the audio element.
-            </audio>
-            <img src={pic3} alt="Image 3" className="audio-image" />
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
